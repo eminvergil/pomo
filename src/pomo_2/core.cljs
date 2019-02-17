61394418cf36bfb73d9d483b1ea85807c64316e3
emocanmimocan <emintrhack@gmail.com>
2019-02-17 16:11:00 +0300

    rest of the files .

--- src/pomo_2/core.cljs
+++ src/pomo_2/core.cljs
@@ -3,23 +3,16 @@
 
 (enable-console-print!)
 
-(println "This text is printed from src/pomo-2/core.cljs. Go ahead and edit it and see reloading in action.")
-
-;; define your app data so that it doesn't get over-written on reload
-
-(defonce app-state (atom {:text "Hello world!"}))
+(def app-state (atom {:text "Example"}))
 
 
 (defn hello-world []
   [:div
    [:h1 (:text @app-state)]
-   [:h3 "Edit this and watch it change!"]])
+   #_[--function--]
+   ])
 
 (reagent/render-component [hello-world]
                           (. js/document (getElementById "app")))
 
-(defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+(defn on-js-reload [])
